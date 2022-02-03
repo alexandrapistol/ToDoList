4f0a03c0364eda45cd56fa2157413df44e774852
Dorin Edu <eduu.dorin@gmail.com>
2022-02-03 15:04:55 +0200

    added todo ops, localstorage

---
+++ src/components/ToDoForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ToDoForm = ({toDoList, setToDoList, input, setInput}) => {
+    const inputHandler = (e) => {
+        setInput(e.target.value)
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if(input == '') return;
+        setToDoList([
+            ...toDoList, 
+            {id: Math.floor(Math.random() * 1000), name: input, isCompleted: false}]
+        )
+        setInput("")
+    }
+    return (
+        <form onSubmit={submitHandler} className="todo-form">
+            <input 
+                type="text"
+                placeholder="Add new todo..."
+                className="todo-input"
+                value={input}
+                onChange={inputHandler}
+            />
+            <button className="todo-button" type="submit">
+                add
+            </button>
+        </form>
+    )
+}
+
+export default ToDoForm;
